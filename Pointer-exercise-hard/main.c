5247109c4f1bb8278dc5d7d53b2530a5b9346513
do-htl <danioberhofer@tsn.at>
2020-10-23 08:35:22 +0200

    Pointer and rle updated

--- Pointer-exercise-hard/main.c
+++ Pointer-exercise-hard/main.c
@@ -1,15 +1,40 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-int main (void)
+int main()
 {
-int i;
-    int * ptr = ( int *) malloc (5 * sizeof ( int ));
-    for (i =0; i <5; i ++)
-    *( ptr + i) = i;
-    printf ( "%d " , * ptr ++);
-    printf ( "%d " , (* ptr )++);
-    printf ( "%d " , * ptr );
-    printf ( "%d " , *++ ptr );
-    printf ( "%d " , ++* ptr );
+    char *c[] = {"GeksQuiz", "MCQ", "TEST", "QUIZ"};
+    char **cp[] = {c + 3, c + 2, c + 1, c};
+    char ***cpp = cp;
+
+    /*
+     * *++cpp   ->  c + 2               // cpp wird permanent um 1 erhöht!!!
+     * *(c+2)   ->  "TEST"
+     */
+    printf("%s ", **++cpp);
+
+    /*
+     * *++cpp           ->  c + 1       // nicht vergessen, cpp wert wurde geändert
+     * *(c+1)           ->  "MCQ"
+     * --"MCQ"          ->  "GeksQuiz"
+     * *"GeksQuiz" + 3  ->  "sQuiz"
+     */
+    printf("%s ", *--*++cpp + 3);
+
+    /*
+     * cpp[-2]      ->  c + 3
+     * *(c+3)       ->  "QUIZ"
+     * "QUIZ" + 3   ->  "Z"
+     */
+    printf("%s ", *cpp[-2] + 3);
+
+    /*
+     * cpp[-1]      ->  c + 2   (-> "TEST")
+     * (c + 2)[-1]  ->  "MCQ"
+     * "MCQ" + 1    ->  "CQ"
+     */
+    printf("%s ", cpp[-1][-1] + 1);
+
+    printf("\n");
+    return 0;
 }
+//  TEST sQuit Z CQ
--- rle-encode/main.c
+++ rle-encode/main.c
@@ -1,16 +1,61 @@
+/*  Author: Oberhofer Daniel
+    Date: 17.10.2020
+    Function: rle-encode
+    rle - run-length encoding
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 
+#define MAX_RLEN 42
+
+/*  run-length encoding algorithm, that works by using
+    multiple for loops, two are used to save a data value
+    to the result and one is used for counting up when the
+    same data value occurs in consecutive data elements
+
+    Using an allocated memory block for the result with the
+    size (length * 2) because when the data value changes allm
+    the time e.g.: FOFO it would be F1O1F1O1
+    resulting in the size being twice as big as the original one
+*/
+
+char* encode(char* str)
+{
+    int rep = 1, cnt, cnt2, step = 0;
+    char temp[MAX_RLEN];
+    int length = strlen(str);
+    char* res = (char*)malloc(sizeof(char) * (length * 2));
+
+    for (cnt= 0; cnt < length; cnt++) {
+        res[step++] = str[cnt];
+        for(;str[cnt] == str[cnt+1];rep++,cnt++) {
+        }
+        sprintf(temp, "%d", rep);
+        rep = 1;
+        for(cnt2=0; *(temp+cnt2);){
+            res[step++] = temp[cnt2++];
+        }
+    }
+    res[step] = '\0';
+    return res;
+}
+
 int main(int argc, char **argv)
 {
-	char str[MAX_RLEN];
+    char str[MAX_RLEN];
 
-       	do {
-		if (fgets(str, MAX_RLEN, stdin) == NULL) break;
-		str[MAX_RLEN] = 0;
-		str[strlen(str)-1] = 0;
-		char* res = encode(str);
-		printf(">%s\n", res);
-		free(res);
-	} while (strlen(str) > 1);
+    do{
+        if(fgets(str, MAX_RLEN, stdin) == NULL) break;
+		str[MAX_RLEN-1] = 0;    // TODO: is this really needed?
+		str[strlen(str)-1] = 0; // TODO: why is this here?
+        char* res = encode(str);
+        printf(">%s\n", res);
+        free(res);
+
+    }while (strlen(str)> 1);
+
+
+
+    return 0;
 }
